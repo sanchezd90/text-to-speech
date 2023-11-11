92c107eeccc2101fe1711db07dbb0f46a630b04d
Daniel <daniel.rueda@ibisdev.tech>
2023-11-11 14:51:20 -0300

    app styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
+// Update your JSX
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Container, Form, Button, Spinner, Card } from 'react-bootstrap';
+import './index.css'
 
 function App() {
   const [prompt, setPrompt] = useState('');
@@ -31,43 +34,53 @@ function App() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter Prompt:
-          <input
-            type="text"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-        </label>
-        <button type="submit" disabled={loading}>
-          Submit
-        </button>
-      </form>
+    <Container className="text-center mt-5" style={{ maxWidth: '50vw' }}>
+    <h1>Text to Speech</h1>
+    <p className="mb-4">Enter a prompt and click 'Submit' to generate and play audio.</p>
 
-      {loading && <p>Loading...</p>}
+    <Form onSubmit={handleSubmit} className="mb-4">
+      <Form.Group controlId="formPrompt" className="mb-3">
+        <Form.Label>Enter Prompt:</Form.Label>
+        <Form.Control
+          type="text"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+      </Form.Group>
+      <Button type="submit" variant="primary" disabled={loading}>
+        {loading ? (
+          <>
+            <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+            Loading...
+          </>
+        ) : (
+          'Submit'
+        )}
+      </Button>
+    </Form>
 
-      {audioFiles.length > 0 && (
-        <div>
-          {/* Displaying the first audio file differently */}
-          <audio controls key={audioFiles[0]}>
-            <source src={`http://localhost:3000/${audioFiles[0]}`} type="audio/wav" />
-            Your browser does not support the audio tag.
-          </audio>
+    {audioFiles.length > 0 && (
+      <div className="mt-4">
+        <h2 className="mb-3">Converted text</h2> {/* Added subtitle for historic audios section */}
+        {/* Displaying the first audio file differently */}
+        <audio controls key={audioFiles[0]} className="audio-control">
+          <source src={`http://localhost:3000/${audioFiles[0]}`} type="audio/wav" />
+          Your browser does not support the audio tag.
+        </audio>
 
-          {/* Displaying the rest of the audio files */}
-          <div>
-            {audioFiles.slice(1).map((audio, index) => (
-              <audio controls key={index + 1}>
-                <source src={`http://localhost:3000/${audio}`} type="audio/wav" />
-                Your browser does not support the audio tag.
-              </audio>
-            ))}
-          </div>
+        <h2 className="mb-3">Previous conversions</h2> {/* Added subtitle for historic audios section */}
+        {/* Displaying the rest of the audio files */}
+        <div className="mt-4">
+          {audioFiles.slice(1).map((audio, index) => (
+            <audio controls key={index + 1} className="audio-control">
+              <source src={`http://localhost:3000/${audio}`} type="audio/wav" />
+              Your browser does not support the audio tag.
+            </audio>
+          ))}
         </div>
-      )}
-    </div>
+      </div>
+    )}
+  </Container>
   );
 }
 
